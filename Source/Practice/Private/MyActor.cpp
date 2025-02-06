0a9c9c77d891d69d5d3b09b16b82d7808530989e
KJH <kim2005kgy@gmail.com>
2025-02-06 22:34:35 +0900

    BeginPlay() 디버깅용 로직 추가. 파일명 수정. 충돌 처리 위해서 디버깅 로직 추가.

--- Source/Practice/Private/MyActor.cpp
+++ Source/Practice/Private/MyActor.cpp
@@ -1,27 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// MyActor.cpp
 
 #include "MyActor.h"
 
-// Sets default values
+#include "Kismet/KismetSystemLibrary.h"
+
 AMyActor::AMyActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false;
 }
 
-// Called when the game starts or when spawned
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
 
-// Called every frame
-void AMyActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+	UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("Source code written by a team member.")));
 }
 
